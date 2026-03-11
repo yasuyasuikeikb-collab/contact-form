87d5f09cf1307f13c182a8886401dcb9f19e4bb4
ikegami yasuki <yasuyasuikeikb@gmail.com>
2026-03-11 23:49:24 +0900

    共通部分layout化

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Contact Form</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+@section('css')
   <link rel="stylesheet" href="{{ asset('css/confirm.css') }}" />
-</head>
+@endsection
 
-<body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
-    </div>
-  </header>
+@section('content')
 
-  <main>
+
     <div class="confirm__content">
       <div class="confirm__heading">
         <h2>お問い合わせ内容確認</h2>
@@ -59,7 +46,4 @@
         </div>
       </form>
     </div>
-  </main>
-</body>
-
-</html>
+@endsection
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Contact Form</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+@section('css')
   <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
-</head>
+@endsection
 
-<body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
-    </div>
-  </header>
+@section('content')
 
-  <main>
+
     <div class="contact-form__content">
       <div class="contact-form__heading">
         <h2>お問い合わせ</h2>
@@ -89,7 +76,4 @@
         </div>
       </form>
     </div>
-  </main>
-</body>
-
-</html>
+    @endsection
